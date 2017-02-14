353f0e72493235fbcbd695cff863a1b17e1b70e8
Samuel Ramos <alu0100815146@ull.edu.es>
2017-02-14 16:38:21 +0000

    Creando y guardando un usuario

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const User = require('./lib/schema');
+
+//Creamos un usuario
+let ramos = User({
+    nombre: "Samuel",
+    apellidos: "Ramos",
+    telefono: 1111
+});
+
+//Lo guardamos
+ramos.save((err) => {
+    if (err) console.log(err);
+
+    console.log('User created!');
+});
